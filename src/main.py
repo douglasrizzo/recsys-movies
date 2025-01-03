e67433d25310b0eff0e89d2317396e3485f6c827
Douglas De Rizzo Meneghetti <douglas.rizzo@orbia.ag>
2025-01-03 16:32:10 -0300

    add optuna

--- src/main.py
+++ src/main.py
@@ -1,28 +1,45 @@
 import argparse
+from functools import partial
 
 import lightning
+import optuna
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks.progress.rich_progress import RichProgressBar
 from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.tuner import Tuner
+from optuna.integration import PyTorchLightningPruningCallback
+from optuna.trial import Trial
 
 from data import UserItemDataModule
 from models import TwoTowerNetwork
 
-if __name__ == "__main__":
-  argparser = argparse.ArgumentParser()
-  argparser.add_argument_group("Training")
-  argparser.add_argument("-F", "--find-lr", action="store_true")
-  argparser.add_argument("-b", "--batch-size", type=int, default=256)
-  argparser.add_argument("-e", "--epochs", type=int, default=10)
-  argparser.add_argument("-v", "--val-check-interval", type=float, default=1 / 100)
-  argparser.add_argument("-p", "--precision", type=str, default="16-mixed")
-  argparser.add_argument("-l", "--limit-val-batches", type=float, default=50)
-  argparser.add_argument("-r", "--lr", type=float, default=1e-3)
-  argparser.add_argument("-t", "--train-workers", type=int, default=4)
-  argparser.add_argument("-w", "--val-workers", type=int, default=1)
 
-  args = argparser.parse_args()
+def objective(
+  trial: Trial | None,
+  train_workers: int,
+  val_workers: int,
+  batch_size: int,
+  precision: str | int,
+  val_check_interval: float | int,
+  epochs: int,
+  limit_val_batches: float | int,
+) -> float:
+  # We optimize the number of layers, hidden units in each layer and dropouts.
+  if trial is not None:
+    n_layers = trial.suggest_int("n_layers", 1, 3)
+    dropout = trial.suggest_float("dropout", 0.2, 0.5)
+    output_dims = [trial.suggest_int(f"n_units_l{i}", 16, 256, log=True) for i in range(n_layers)]
+    callbacks = [
+      PyTorchLightningPruningCallback(trial, monitor="val_acc"),
+      RichProgressBar(),
+    ]
+  else:
+    output_dims = [64, 32]
+    dropout = 0.5
+    callbacks = [
+      EarlyStopping(monitor="Loss/MSE Val", mode="min"),
+      RichProgressBar(),
+    ]
+    n_layers = len(output_dims)
 
   datamodule = UserItemDataModule(
     "data/movie_features.parquet",
@@ -30,22 +47,74 @@ if __name__ == "__main__":
     "data/ratings.parquet",
     "data/movie_features_to_normalize.txt",
     "data/user_features_to_normalize.txt",
-    train_workers=args.train_workers,
-    val_workers=args.val_workers,
-    batch_size=args.batch_size,
+    train_workers=train_workers,
+    val_workers=val_workers,
+    batch_size=batch_size,
   )
-  model = TwoTowerNetwork(lr=args.lr)
+  model = TwoTowerNetwork(output_dims=output_dims, dropout=dropout)
 
   wandb_logger = WandbLogger(project="recsys-movies")
   trainer = lightning.Trainer(
+    enable_checkpointing=False,
+    precision=precision,
+    val_check_interval=val_check_interval,
+    logger=wandb_logger,
+    max_epochs=epochs,
+    limit_val_batches=limit_val_batches,
+    callbacks=callbacks,
+    accelerator="auto",
+    devices=1,
+  )
+  hyperparameters = {"n_layers": n_layers, "dropout": dropout, "output_dims": output_dims}
+  trainer.logger.log_hyperparams(hyperparameters)
+  trainer.fit(model=model, datamodule=datamodule)
+  return trainer.callback_metrics["Loss/MSE Val"].item()
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument_group("Training")
+  parser.add_argument("-b", "--batch-size", type=int, default=256)
+  parser.add_argument("-e", "--epochs", type=int, default=10)
+  parser.add_argument("-v", "--val-check-interval", type=float, default=1 / 100)
+  parser.add_argument("-p", "--precision", type=str, default="16-mixed")
+  parser.add_argument("-l", "--limit-val-batches", type=float, default=50)
+  parser.add_argument("-r", "--lr", type=float, default=1e-3)
+  parser.add_argument("-t", "--train-workers", type=int, default=4)
+  parser.add_argument("-w", "--val-workers", type=int, default=1)
+  parser.add_argument_group("Optuna")
+  parser.add_argument("-o", "--optuna", action="store_true", help="Activate the optuna feature.")
+  parser.add_argument(
+    "-P",
+    "--pruning",
+    action="store_true",
+    help="Activate the pruning feature. `MedianPruner` stops unpromising trials at the early stages of training.",
+  )
+
+  args = parser.parse_args()
+  objective_partial = partial(
+    objective,
+    train_workers=args.train_workers,
+    val_workers=args.val_workers,
+    batch_size=args.batch_size,
     precision=args.precision,
     val_check_interval=args.val_check_interval,
-    logger=wandb_logger,
-    max_epochs=args.epochs,
+    epochs=args.epochs,
     limit_val_batches=args.limit_val_batches,
-    callbacks=[EarlyStopping(monitor="Loss/MSE Val", mode="min"), RichProgressBar()],
   )
-  if args.find_lr:
-    tuner = Tuner(trainer)
-    tuner.lr_find(model, datamodule=datamodule)
-  trainer.fit(model=model, datamodule=datamodule)
+
+  if args.optuna:
+    pruner = optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
+    study = optuna.create_study(direction="minimize", pruner=pruner)
+    study.optimize(objective_partial, n_trials=100, timeout=600)
+
+    print(f"Number of finished trials: {len(study.trials)}")
+    print("Best trial:")
+    trial = study.best_trial
+    print(f"  Value: {trial.value}")
+    print("  Params: ")
+    for key, value in trial.params.items():
+      print(f"    {key}: {value}")
+
+  else:
+    objective_partial(None)
--- src/models.py
+++ src/models.py
@@ -16,24 +16,116 @@ class TwoTowerNetwork(lightning.LightningModule):
       item_tower: The item tower neural network.
   """
 
-  def __init__(self, lr: float = 1e-3, dropout: float = 0.5, n_features: int = 128, n_layers: int = 3) -> None:
-    """Initialize a two-tower neural network for user-item recommendation."""
+  def __init__(
+    self,
+    output_dims: list[int] | None = None,
+    dropout: float = 0.5,
+    lr: float = 1e-3,
+  ) -> None:
+    """Initialize a two-tower neural network for user-item recommendation.
+
+    The two-tower neural network consists of two towers: the user tower and the
+    item tower. The user tower processes the user features and the item tower
+    processes the item features. The final prediction is computed by taking the
+    dot product of the two tower outputs.
+
+    Parameters
+    ----------
+    output_dims : list[int], optional
+        The output dimensions of the user and item towers. If `None`, the
+        default output dimensions are used. The default is `None`.
+    dropout : float, optional
+        The dropout probability. The default is 0.5.
+    lr : float, optional
+        The learning rate. The default is 1e-3.
+    """
+    if output_dims is None:
+      output_dims = [64, 32]
     super().__init__()
     # The user tower processes the user features
-    self.user_tower = self.make_tower(dropout, n_features, n_layers)
+    self.user_tower = self.make_tower(dropout, output_dims)
     # The item tower processes the item features
-    self.item_tower = self.make_tower(dropout, n_features, n_layers)
+    self.item_tower = self.make_tower(dropout, output_dims)
 
     self.lr = lr
 
   @staticmethod
-  def make_tower(dropout, n_features, n_layers):
+  def make_tower(dropout: float, output_dims: list[int]) -> nn.Sequential:
+    """Construct a neural network tower.
+
+    Parameters
+    ----------
+    dropout : float
+        The dropout probability.
+    output_dims : list[int]
+        The number of neurons in each layer.
+
+    Returns
+    -------
+    nn.Sequential
+        The neural network tower.
+    """
     layers = []
-    for _ in range(n_layers):
-      layers.extend([nn.LazyLinear(out_features=n_features), nn.Tanh()])
-      if _ != n_layers - 1:
-        layers.append(nn.Dropout(p=dropout))
+    for layer_num, output_dim in enumerate(output_dims):
+      layers.extend((
+        (
+          nn.LazyLinear(out_features=output_dim)
+          if layer_num == 0
+          else nn.Linear(
+            in_features=output_dims[layer_num - 1],
+            out_features=output_dim,
+          )
+        ),
+        nn.Tanh(),
+      ))
+      if layer_num != len(output_dims) - 1:
+        layers.append(
+          nn.Dropout(p=dropout),
+        )
     return nn.Sequential(*layers)
+
+  def forward(self, user_features: torch.Tensor, item_features: torch.Tensor) -> torch.Tensor:
+    """Perform a forward pass through the two-tower network.
+
+    This function processes the user and item features through their respective
+    towers and computes the dot product of the resulting embeddings.
+
+    Parameters
+    ----------
+    user_features : torch.Tensor
+        The input tensor representing user features.
+    item_features : torch.Tensor
+        The input tensor representing item features.
+
+    Returns
+    -------
+    torch.Tensor
+        The predicted interaction score for each user-item pair.
+    """
+    user_embedding = self.user_tower(user_features)
+    item_embedding = self.item_tower(item_features)
+    return (user_embedding * item_embedding).sum(dim=1)
+
+  def inference(self, user_features: torch.Tensor, item_features: torch.Tensor) -> torch.Tensor:
+    """Compute the interaction scores between all users and items.
+
+    Parameters
+    ----------
+    user_features : torch.Tensor
+        The input tensor representing user features.
+    item_features : torch.Tensor
+        The input tensor representing item features.
+
+    Returns
+    -------
+    torch.Tensor
+        A tensor of shape (num_users, num_items) where each element at
+        index (i, j) represents the predicted interaction score between the
+        i-th user and the j-th item.
+    """
+    user_embedding = self.user_tower(user_features)
+    item_embedding = self.item_tower(item_features)
+    return user_embedding @ item_embedding.T
 
   def compute_loss(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> torch.Tensor:
     """Compute the loss for the given batch.
@@ -45,35 +137,47 @@ class TwoTowerNetwork(lightning.LightningModule):
         torch.Tensor: The loss for the current batch.
     """
     user_features, item_features, rating = batch
-    user_embedding = self.user_tower(user_features)
-    item_embedding = self.item_tower(item_features)
-    y_hat = (user_embedding * item_embedding).sum(dim=1)
+    y_hat = self.forward(user_features, item_features)
     return nn.functional.mse_loss(y_hat, rating), (y_hat - rating).abs().mean()
 
   def training_step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
     """Compute the loss for the given batch.
 
-    Args:
-        batch: A tuple of three tensors: user features, item features, and ratings.
-        batch_idx: The index of the current batch (unused).
+    Parameters
+    ----------
+    batch : tuple of torch.Tensor
+        A tuple containing three tensors: user features, item features, and ratings.
+    batch_idx : int
+        The index of the current batch (unused).
 
-    Returns:
-        torch.Tensor: The loss for the current batch.
+    Returns
+    -------
+    torch.Tensor
+        The loss for the current batch.
     """
     mse, mae = self.compute_loss(batch)
     self.log("Loss/MSE Train", mse)
     self.log("Loss/MAE Train", mae)
     return mse
 
-  def validation_step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
+  def validation_step(
+    self,
+    batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+    batch_idx: int,
+  ) -> torch.Tensor:
     """Compute the loss for the given batch.
 
-    Args:
-        batch: A tuple of three tensors: user features, item features, and ratings.
-        batch_idx: The index of the current batch (unused).
+    Parameters
+    ----------
+    batch : tuple of torch.Tensor
+        A tuple containing three tensors: user features, item features, and ratings.
+    batch_idx : int
+        The index of the current batch (unused).
 
-    Returns:
-        torch.Tensor: The loss for the current batch.
+    Returns
+    -------
+    torch.Tensor
+        The loss for the current batch.
     """
     mse, mae = self.compute_loss(batch)
     self.log("Loss/MSE Val", mse)
@@ -83,7 +187,9 @@ class TwoTowerNetwork(lightning.LightningModule):
   def configure_optimizers(self) -> torch.optim.Adam:
     """Configure the optimizer for the model.
 
-    Returns:
-        torch.optim.Adam: The optimizer for training the model.
+    Returns
+    -------
+    torch.optim.Adam
+        The optimizer for training the model.
     """
     return torch.optim.Adam(self.parameters(), lr=(self.lr or self.learning_rate))
